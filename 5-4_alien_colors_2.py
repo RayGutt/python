7c8c5d43224003fb1a8a6ff517e82131ed20dae2
parisCycleGeek <le.caribou@gmail.com>
2019-12-20 16:39:32 +0100

    new exercises

---
+++ 5-4_alien_colors_2.py
@@ -0,0 +1,7 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+	print("the player just earned 5 points for shooting the alien.")
+else:
+	print("the player just earned 10 points.")
+
---
+++ 5-7_favorite_fruit.py
@@ -0,0 +1,7 @@
+favorite_fruits = ['banana', 'apple', 'pear']
+
+if 'banana' in favorite_fruits:
+	print("You really like bananas!")
+
+if 'mango' in favorite_fruits:
+	print("You really like mangos")
